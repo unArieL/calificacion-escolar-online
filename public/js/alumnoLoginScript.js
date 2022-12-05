94981873212d12ce50d33bc2129dcb5eab3aa553
ArieL <alcarazsincuenta@gmail.com>
2022-12-05 02:53:48 -0300

    vista LOGIN de alumno terminado

---
+++ public/js/alumnoLoginScript.js
@@ -0,0 +1,10 @@
+const btnBackMain = document.querySelector('#btn-back')
+const imgEscudo = document.querySelector('#escudo-header')
+
+btnBackMain.onclick = () => {
+    window.location = '/'
+}
+
+imgEscudo.onclick = () => {
+    window.location = '/'
+}
